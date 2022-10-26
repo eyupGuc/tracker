e89b9c82c16530619d31caff0b58ac888ad6d1e1
eyupGuc <meydanguresi@gmail.com>
2022-10-26 09:54:39 +0300

    m

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="header ">
       <h1 className="display-4 ">TASK TRACKER</h1>
-      <button className="btn btn-success">Close Add Task Bar</button>
+    
     </div>
   );
 };
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,40 @@
-const Home=()=>{
-    return(
-        <div>home</div>
-    )
-}
+import axios from "axios";
+import  Button  from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [task, setTask] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Show Add Task Bar");
+  const url = "https://6358cfefc26aac906f48b852.mockapi.io/api1/tasks";
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const buttonText = isOpen ? "Show Add Task Bar" : "Close Add Task Bar";
+    setText(buttonText);
+  };
+
+  const getTask = async () => {
+    const { data } = await axios(url);
+    setTask(data);
+  };
+
+  useEffect(() => {
+    getTask();
+    console.log(task);
+  }, []);
+
+  return (
+    <div>
+      <Button 
+        onClick={() => {
+          toggle();
+        }}
+      >
+        {text}
+      </Button>
+    </div>
+  );
+};
 
 export default Home;
