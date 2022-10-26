ae7280de526dbc4a7cf042379a6dedf92ee70842
eyupGuc <meydanguresi@gmail.com>
2022-10-26 14:40:26 +0300

    m

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 const AddTask = () => {
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { task, date };
+    console.log(task);
+    console.log(date);
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Task</Form.Label>
-          <Form.Control type="text" placeholder="Enter A Task" />
+          <Form.Control
+            type="text"
+            placeholder="Enter A Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Date</Form.Label>
-          <Form.Control type="date" />
+          <Form.Control
+            type="date"
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
         </Form.Group>
-        <Button variant="success" type="submit">SAVE </Button>
+        <Button variant="success" type="submit">
+          SAVE{" "}
+        </Button>
       </Form>
     </div>
   );
