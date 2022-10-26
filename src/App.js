44bc8703bed0d9f1c430f2dc01696a769c7b83ec
eyupGuc <meydanguresi@gmail.com>
2022-10-26 09:55:05 +0300

    m

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./page/Home";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container text-center">
       <Header />
       <Home />
     </div>
